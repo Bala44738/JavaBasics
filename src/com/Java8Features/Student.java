a1ebf317e8ee6eb1e28246e1b68833834bcbe6f7
Bala44738 <121427026+Bala44738@users.noreply.github.com>
2023-05-24 16:45:13 +0530

    Added new files

--- src/com/Java8Features/Student.java
+++ src/com/Java8Features/Student.java
@@ -2,59 +2,76 @@ package com.Java8Features;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
-public class Student {
+public class Student implements Comparable{
     int id;
     String name;
     int marks;
     String grade;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getMarks() {
+        return marks;
+    }
+
+    public void setMarks(int marks) {
+        this.marks = marks;
+    }
+
+    public String getGrade() {
+        return grade;
+    }
+
+    public void setGrade(String grade) {
+        this.grade = grade;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Student student = (Student) o;
+        return id == student.id && marks == student.marks && Objects.equals(name, student.name) && Objects.equals(grade, student.grade);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, marks, grade);
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", marks=" + marks +
+                ", grade='" + grade + '\'' +
+                '}';
+    }
+
+    @Override
+    public int compareTo(Object o) {
+        Student s= (Student) o;
+//        return Integer.compare(this.marks,s.marks);
+
+        return this.name.compareTo(s.name);
+    }
 }
 
-     class TestStudent{
-         public static void main(String[] args) {
-             List<Student> al=new ArrayList<>();
-             Student s1 = new Student();
-             s1.id=1001;
-             s1.name="Prasad";
-             s1.marks=93;
-
-             Student s2 = new Student();
-             s2.id=1002;
-             s2.name="Srinu";
-             s2.marks=91;
-
-             Student s3 = new Student();
-             s3.id=1003;
-             s3.name="Bala";
-             s3.marks=78;
-
-             Student s4 = new Student();
-             s4.id=1004;
-             s4.name="Naidu";
-             s4.marks=98;
-
-             al.add(s1);
-             al.add(s2);
-             al.add(s3);
-             al.add(s4);
-
-//           for (Student stu:al){
-//               System.out.println(stu.id+" "+stu.name);
-//
-//           }
-
-             al.stream().map(stu->{
-                 if(stu.marks>90){
-                     stu.grade="A";
-                 }else {
-                     stu.grade="B";
-                 }
-                 return stu;
-             }).filter(stu->stu.grade.equals("A")).forEach(x->{
-                 System.out.println(x.id);
-                 System.out.println(x.name);
-                 System.out.println(x.marks);
-                 System.out.println("  ");
-             });
-         }
-     }
---
+++ src/com/Java8Features/TestStudent3.java
@@ -0,0 +1,69 @@
+package com.Java8Features;
+
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class TestStudent3 {
+
+    public static void main(String[] args) {
+        List<Student> al = new ArrayList<>();
+        Student s1 = new Student();
+        s1.id = 1001;
+        s1.name = "Prasad";
+        s1.marks = 93;
+
+        Student s2 = new Student();
+        s2.id = 1002;
+        s2.name = "Srinu";
+        s2.marks = 91;
+
+        Student s3 = new Student();
+        s3.id = 1003;
+        s3.name = "Bala";
+        s3.marks = 78;
+
+        Student s4 = new Student();
+        s4.id = 1004;
+        s4.name = "Naidu";
+        s4.marks = 98;
+
+        al.add(s1);
+        al.add(s2);
+        al.add(s3);
+        al.add(s4);
+
+       // Function interface
+        Function<Student,Student> mapfun = (x)->{
+            if (x.getMarks()>90) {
+                x.grade="A";
+            }else {
+                x.grade="B";
+            }
+            return x;
+        };
+
+       // Predicate
+        Predicate<Student> filtrPre = x->x.getGrade().equals("A");
+
+        // Comparable
+//        Comparable<Student> compsrt = x->{
+//
+//            return 0;
+//        };
+
+        // Comparator
+        Comparator<Student> compsrt = (x,y)->{
+            return x.getName().compareTo(y.getName());
+        };
+
+        // Consumer
+        Consumer<Student> cunforech = x-> System.out.println(x.getId()+" "+x.getName()+
+                " "+x.getMarks()+" "+x.getGrade());
+
+        al.stream().map(mapfun).filter(filtrPre).sorted().forEach(cunforech);
+    }
+}
